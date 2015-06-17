67c444e1887f3b4d6184e8a0e37222582b229c83
nrpatten <nrpatten@gmail.com>
2015-06-17 23:50:03 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CoinMarketCapAPISite 
+-------------
+
+* Simple CoinMarketCap API website made with jquery, dataTables and bootstrap
+ * Pulls all of the Bittrex Coin data and displays it in a table
+ * Auto refresh the data every 5 minutes
+ * Bootstrap Style with Pagination ans Search
+ * Print or Copy and Download as CSV, Excel, PDF
---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	var refreshTable = $("#coinmarketcap").DataTable( {
+		"ajax": {
+			"url": "http://162.243.239.186:8000/?url=http://coinmarketcap.northpole.ro/api/v5/all.json",
+			"dataSrc": "markets"
+		},
+		"columns": [
+			{ "data": "position", "render": function (data,type,row) { return '<img src="img/HashMark.png" height="14" width="14"></img> ' + (data) +''; } },
+			{ "data": "name", "render": function (data,type,row) { return '<img src="img/'+ (data).toLowerCase() +'.png"></img> ' + (data) +''; } },
+                        { "data": "symbol" },
+                        { "data": "marketCap.usd", "render": function (data,type,row) { return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toLocaleString() +''; } },
+                        { "data": "price.usd", "render": function (data,type,row) { return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toFixed(8) +''; } },
+                        { "data": "price.btc", "render": function (data,type,row) { return '<span style="font-size:12px;color:orange;" class="glyphicon glyphicon-bitcoin"></span> ' + Number(data).toFixed(8) +''; } },
+                        { "data": "volume24.btc", "render": function (data,type,row) { return '<span style="font-size:12px;color:orange;" class="glyphicon glyphicon-bitcoin"></span> ' + Number(data).toFixed(2) +''; } }
+                ],
+                "dom": 'T<"clear">lfrtip',
+                "tableTools": {
+                    "sSwfPath": "swf/copy_csv_xls_pdf.swf",
+                    "aButtons": [
+                        "copy",
+                        "print",
+                        {
+                            "sExtends":    "collection",
+                            "sButtonText": "Save",
+                            "aButtons":    [ "csv", "xls", "pdf" ]
+                        }
+                    ]
+                }
+	} );
+        function round(value, decimals) {
+            return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+        }
+        setInterval (function test() {
+          refreshTable.ajax.reload( null, false );
+        }, 300000);
+} );
