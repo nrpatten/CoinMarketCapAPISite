94331834623932413be623ba80fe729d70a16f49
nrpatten <nrpatten@gmail.com>
2015-06-29 00:57:15 +1000

    Test Branch

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # CoinMarketCapAPISite 
 -------------
 
-* Simple CoinMarketCap API website made with jquery, dataTables and bootstrap
- * Pulls all of the CoinMarketCap data and displays it in a table
- * Auto refresh the data every 5 minutes
- * Bootstrap Style with Pagination ans Search
- * Print or Copy and Download as CSV, Excel, PDF
+* Simple CoinMarketCap API website made with jquery, dataTables, Bootstrap and PHP Simple html Dom
+ * Scrapes coinmarketcap every 5 minuts with PHP Dom.
+ * Pulls all of the CoinMarketCap data and displays it in a table.
+ * Auto refresh the data every 5 minutes.
+ * Bootstrap Style with Pagination and Search.
+ * Print or Copy and Download as CSV, Excel, PDF.
+
+Note PHP5 must be used to run PHP Simple html Dom.
+
+Setup:
+`cd /var/www`
+`git clone https://github.com/nrpatten/CoinMarketCapAPISite.git coinmarketcap`
+`cd json`
+`chmod +x check.sh`
+
+Crontab:
+```shell
+crontab -e
+```
+
+Add:
+```shell
+*/5 * * * * cd /var/www/coinmarketcap/json/ && /usr/bin/php allcoins.php > all.json
+*/1 * * * * cd /var/www/coinmarketcap/json/ && /bin/sh check.sh all.json
+```
+Done..
--- js/app.js
+++ js/app.js
@@ -1,89 +1,45 @@
-var $th = $("<td></td>");
-
 $(document).ready(function() {
-
-  var myTable = $("#coinmarketcap").on('init.dt', getData).DataTable({
-    "processing": true,
-    "ajax": {
-      "url": "http://162.243.239.186:8000/?url=http://coinmarketcap.northpole.ro/api/v5/all.json",
-      "dataSrc": "markets"
-    },
-    "columns": [
-    { "data": "position",
-      "render": function(data, type, row) {
-        return '<img src="img/HashMark.png" height="14" width="14"></img> ' + (data) + ' ';
+   var refreshTable = $("#coinmarketcap").DataTable( {
+      "bRetrieve": true,
+      "processing": true,
+      "language": {
+         "emptyTable": "The table is still loading data or there was an error with JSON"
+      },
+      "ajax": {
+         "url": "json/markets.json",
+         "dataSrc": "markets",
+         "dataType": "json"
+      },
+      "columns": [
+         { "data": "position", "render": function (data,type,row) { return '<img src="img/HashMark.png" height="14" width="14"></img> ' + (data) +''; } },
+         { "data": null, "render": function (data,type,row) { return '<img src="http://coinmarketcap.com/static/img/coins/16x16/'+ (data.logo) +'" height="18" width="18"></img> ' + (data.name) +''; } },
+         { "data": "symbol" },
+         { "data": "marketcap_usd", "render": function (data,type,row) { return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toLocaleString() +''; } },
+         { "data": "price_usd", "render": function (data,type,row) { return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toFixed(8) +''; } },
+         { "data": "supply_btc", "render": function (data,type,row) { return '<span style="font-size:12px;color:orange;" class="glyphicon glyphicon-bitcoin"></span> ' + Number(data).toFixed(8) +''; } },
+         { "data": "volume_usd" , "render": function (data,type,row) { return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toLocaleString() +''; } },
+         { "data": "change1h" },
+         { "data": "change24h" },
+         { "data": "change1d" }
+      ],
+      "dom": 'T<"clear">lfrtip',
+      "tableTools": {
+         "sSwfPath": "swf/copy_csv_xls_pdf.swf",
+         "aButtons": [
+            "copy",
+            "print",
+            {
+            "sExtends": "collection",
+            "sButtonText": "Save",
+            "aButtons": [ "csv", "xls", "pdf" ]
+            }
+         ]
       }
-    }, 
-    { "data": null,
-      render: function( data, type, row) {
-        return (data.name); $th.wrap('<div></div>').parent().html();
-      }
-    },
-    { "data": "symbol" }, 
-    { "data": "marketCap.usd",
-      "render": function(data, type, row) {
-        return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toLocaleString() + ' ';
-      }
-    },
-    { "data": "price.usd",
-      "render": function(data, type, row) {
-        return '<span style="font-size:12px;" class="glyphicon glyphicon-usd"></span> ' + Number(data).toFixed(8) + ' ';
-      }
-    },
-    { "data": "price.btc",
-      "render": function(data, type, row) {
-        return '<span style="font-size:12px;color:orange;" class="glyphicon glyphicon-bitcoin"></span> ' + Number(data).toFixed(8) + ' ';
-      }
-    }, 
-    { "data": "availableSupply" },
-    { "data": "volume24.btc",
-      "render": function(data, type, row) {
-        return '<span style="font-size:12px;color:orange;" class="glyphicon glyphicon-bitcoin"></span> ' + Number(data).toLocaleString() + ' ';
-      }
-    }
-    ],
-    "dom": 'T<"clear">lfrtip',
-    "tableTools": {
-      "sSwfPath": "swf/copy_csv_xls_pdf.swf",
-      "aButtons": [
-        "copy",
-        "print", {
-          "sExtends": "collection",
-          "sButtonText": "Save",
-          "aButtons": ["csv", "xls", "pdf"]
-        }
-      ]
-    }
-  });
-  function find(key, obj) {
-    for (var i in obj) {
-      if (obj[i].name == key) {
-        return obj[i];
-      }
-    }
-   return obj[i];
-  }
-  function getData() {
-    var myTable = $("#coinmarketcap").DataTable();
-    $.ajax({
-      url: "quick_search.json",
-      dataType: "json",
-      success: function(json) {
-      myTable
-        .column(1)
-        .nodes()
-        .each(function(node, index, dt) {
-          var slug = find(myTable.cell(node).data().name, json);
-          var item = ('  <img src="img/'+ (slug.slug) +'.png" height="18" width="18" />');
-          $(myTable.cell(node).node()).append(item);
-        });
-      }
-    });
-  }
-  function round(value, decimals) {
-    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
-  }
-  setInterval(function test() {
-    myTable.ajax.reload(null, false);
-  }, 300000);
+   });
+   function round(value, decimals) {
+      return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+   }
+   setInterval (function test() {
+      refreshTable.ajax.reload( null, false );
+   }, 300000);
 });
